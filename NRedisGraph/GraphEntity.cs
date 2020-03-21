83ab635b4c673ed04bf43580fa5dd7e5e48d646c
Tom Hanks <tommy.hanks@gmail.com>
2020-03-21 06:45:02 -0400

    Added documentation here.

--- NRedisGraph/GraphEntity.cs
+++ NRedisGraph/GraphEntity.cs
@@ -3,21 +3,56 @@ using System.Collections.Generic;
 
 namespace NRedisGraph
 {
+    /// <summary>
+    /// An abstract representation of a graph entity.
+    /// 
+    /// A graph entity has an ID and a set of properties. The properties are mapped and accessed by their names.
+    /// </summary>
     public abstract class GraphEntity
     {
+        /// <summary>
+        /// The ID of the entity.
+        /// </summary>
+        /// <value></value>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The collection of properties associated with an entity.
+        /// </summary>
+        /// <returns></returns>
         public IDictionary<string, Property> PropertyMap = new Dictionary<string, Property>();
 
+        /// <summary>
+        /// Add a property to the entity.
+        /// </summary>
+        /// <param name="name">Name of the property.</param>
+        /// <param name="value">Value of the property.</param>
         public void AddProperty(string name, object value) =>
             AddProperty(new Property(name, value));
 
+        /// <summary>
+        /// Add a property to the entity.
+        /// </summary>
+        /// <param name="property">The property to add.</param>
         public void AddProperty(Property property) => PropertyMap.Add(property.Name, property);
 
+        /// <summary>
+        /// Remove a property from the entity by name.
+        /// </summary>
+        /// <param name="name"></param>
         public void RemoveProperty(string name) => PropertyMap.Remove(name);
 
+        /// <summary>
+        /// How many properties does this entity have?
+        /// </summary>
         public int NumberOfProperties => PropertyMap.Count;
 
+        /// <summary>
+        /// Overriden Equals that considers the equality of the entity ID as well as the equality of the
+        /// properties that each entity has.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
             if (this == obj)
@@ -33,6 +68,11 @@ namespace NRedisGraph
             return Id == that.Id && PropertyMap.SequenceEqual(that.PropertyMap);
         }
 
+        /// <summary>
+        /// Overriden GetHashCode that computes a deterministic hash code based on the value of the ID
+        /// and the name/value of each of the associated properties.
+        /// </summary>
+        /// <returns></returns>
         public override int GetHashCode()
         {
             unchecked
@@ -51,6 +91,10 @@ namespace NRedisGraph
             }
         }
 
+        /// <summary>
+        /// Overriden ToString that emits a string containing the ID and property map of the entity.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             var sb = new StringBuilder();
