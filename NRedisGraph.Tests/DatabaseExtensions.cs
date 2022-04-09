386d0903d606474cc0194edc114f390c9895b399
Tommy Hanks <tommy.hanks@gmail.com>
2022-04-09 07:00:09 -0400

    Added extension method to IDatabase which provides "support" for the LOLWUT command.

---
+++ NRedisGraph.Tests/DatabaseExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace NRedisGraph.Tests
+{
+    public class IDatabaseExtensionsTests : BaseTest
+    {
+        private ConnectionMultiplexer _muxr;
+        private IDatabase _db;
+        protected override void BeforeTest()
+        {
+            _muxr = ConnectionMultiplexer.Connect("localhost");
+            _db = _muxr.GetDatabase(0);
+        }
+
+        protected override void AfterTest()
+        {
+            _muxr.Dispose();
+            _muxr = null;
+        }
+
+        [Fact]
+        public void CanLolWut()
+        {
+            var result = _db.LolWut();
+            
+            Assert.NotNull(result.ToString());
+        }
+
+        [Fact]
+        public void CanLolWutWithParams()
+        {
+            var result = _db.LolWut(1, 2, 3, 4, 5);
+            
+            Assert.NotNull(result.ToString());
+        }
+
+        [Fact]
+        public async Task CanLolWutAsync()
+        {
+            var result = await _db.LolWutAsync();
+            
+            Assert.NotNull(result.ToString());
+        }
+
+        [Fact]
+        public async Task CanLolWutWithParamsAsync()
+        {
+            var result = await _db.LolWutAsync(1, 2, 3, 4, 5);
+            
+            Assert.NotNull(result.ToString());
+        }
+    }
+}
